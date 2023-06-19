4087cc6d730d52937e8458a38933db92f53a59fe
mavludacodes <mavluda.coder@gmail.com>
2023-06-20 02:19:30 +0500

    reserve room started

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -14,6 +14,8 @@ export default function Room() {
     });
   }, []);
 
+  const [open, setOpen] = useState(true);
+
   return (
     <>
       <div className="ml-32  mt-16">
@@ -62,7 +64,10 @@ export default function Room() {
               </p>
             </div>
             <div className="">
-              <button className=" bg-black   py-1 px-8 text-white text-base   font-semibold">
+              <button
+                onClick={() => setOpen(true)}
+                className=" bg-black   py-1 px-8 text-white text-base   font-semibold"
+              >
                 Reserve
               </button>
             </div>
@@ -145,6 +150,92 @@ export default function Room() {
           </div>
         </div>
       </div>
+
+      {/* Modal */}
+      {open && (
+        <div
+          className="relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          {/* <!--
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0"
+  --> */}
+          <div className=" fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <div className="mt-2">
+                        <div>
+                          <label>Name:</label>
+                          <input
+                            type="text"
+                            className="mb-2 block w-full  border-0 py-3.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Name"
+                            // value={searchInput}
+                            // onChange={(e) => setSearchInput(e.target.value)}
+                          />
+                          <label className="mb-2">Date:</label>
+                          <input
+                            type="date"
+                            className=" mb-2 block w-full  border-0 py-3.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Name"
+                            // value={searchInput}
+                            // onChange={(e) => setSearchInput(e.target.value)}
+                          />
+                          <label>Start:</label>
+                          <input
+                            type="time"
+                            className="mt-2 block w-full  border-0 py-3.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Name"
+                            // value={searchInput}
+                            // onChange={(e) => setSearchInput(e.target.value)}
+                          />
+                          <label className="mt-4">End:</label>
+                          <input
+                            type="time"
+                            className=" mb-2 block w-full  border-0 py-3.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Name"
+                            // value={searchInput}
+                            // onChange={(e) => setSearchInput(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                  >
+                    Reserve
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => setOpen(false)}
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
