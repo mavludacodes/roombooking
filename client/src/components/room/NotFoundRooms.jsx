812df5017c9b33d233d24f4ac0373e64ae993d36
mavludacodes <mavluda.coder@gmail.com>
2023-08-11 15:50:41 +0500

    feat: add loader and not found page

---
+++ client/src/components/room/NotFoundRooms.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFoundRooms() {
+  return (
+    <div className="mt-40 md:mt-28 mb-28">
+      <p className="text-2xl text-center">Sorry, no results...</p>
+    </div>
+  );
+}
+
+export default NotFoundRooms;
